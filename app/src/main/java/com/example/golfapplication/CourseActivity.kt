85f22ae81f58388d47c099e86a017d68917bd1bc
Asim Ghani <ghaniasimm@gmail.com>
2019-04-25 17:28:24 +0300

    Update CourseActivity.kt

--- app/src/main/java/com/example/golfapplication/CourseActivity.kt
+++ app/src/main/java/com/example/golfapplication/CourseActivity.kt
@@ -28,6 +28,29 @@ class CourseActivity : AppCompatActivity() {
             descriptionTextView.text = course["text"].toString()
             webTextView.text = course["web"].toString()
             Glide.with(imageView.context).load("http://ptm.fi/materials/golfcourses/"+course["image"]).into(imageView)
+            
+            val textView: TextView = findViewById(R.id.locationTextView)
+
+            textView.setOnClickListener{
+                // get latitude and longitude values
+                val lat = course["lat"].toString().toDouble()
+                val lng = course["lng"].toString().toDouble()
+
+                // Build the intent
+                val location = Uri.parse("geo:$lat,$lng")
+                val mapIntent = Intent(Intent.ACTION_VIEW, location)
+
+                // Verify it resolves
+                val activities: List<ResolveInfo> = packageManager.queryIntentActivities(mapIntent, 0)
+                val isIntentSafe: Boolean = activities.isNotEmpty()
+
+                // Start an activity if it's safe
+                if (isIntentSafe) {
+                    startActivity(mapIntent)
+                } else {
+                    Toast.makeText(this, "There is no activity to handle map intent!", Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
